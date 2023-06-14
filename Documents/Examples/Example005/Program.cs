a69cc56ea2c6e9dbe480f0106ab2547f7a92e172
AnnaTsygalnyuk <nika314@list.ru>
2023-06-14 23:11:54 +0200

    If-else

---
+++ Documents/Examples/Example005/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+   Console.WriteLine("Привет, " + username); 
+}
