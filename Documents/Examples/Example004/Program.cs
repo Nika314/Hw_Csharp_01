7813d5633401d2254743738ea9f249514d8eeee2
AnnaTsygalnyuk <nika314@list.ru>
2023-06-14 17:29:37 +0200

    Random

--- Documents/Examples/Example004/Program.cs
+++ Documents/Examples/Example004/Program.cs
@@ -1,4 +1,6 @@
-﻿double numberA = 30;
-double numberB = 6;
+﻿double numberA = new Random().Next(1, 10); // 1, 2, 3,.. , 9
+Console.WriteLine(numberA);
+double numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 double result = numberA / numberB;
 Console.WriteLine(result);
