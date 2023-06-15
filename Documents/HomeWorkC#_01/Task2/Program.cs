a4025bc5d01de5c47548f34483de266cdb86b277
AnnaTsygalnyuk <nika314@list.ru>
2023-06-15 22:02:17 +0200

    Task2

---
+++ Documents/HomeWorkC#_01/Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Введите два числа: ");
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+
+if(a > b)
+{
+  Console.WriteLine("max = "+ a + "  min = " + b);  
+}
+else if(a < b)
+{
+    Console.WriteLine("max = "+ b + "  min = " + a);
+}
+else Console.WriteLine("Числа равны");
