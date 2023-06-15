63ee3585b2a502e0fbd8519ba9760cbf309cdad6
AnnaTsygalnyuk <nika314@list.ru>
2023-06-15 23:20:17 +0200

    Task8

---
+++ Documents/HomeWorkC#_01/Task8/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+for(int even_numbers = 1; even_numbers <= number; even_numbers++)
+{
+    if(even_numbers % 2 == 0)
+    {
+        Console.Write(even_numbers + " ");        
+    }  
+}
+
