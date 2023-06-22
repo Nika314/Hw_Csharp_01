3a4eca776c8a735856c3c24b99822f49e99b2855
AnnaTsygalnyuk <nika314@list.ru>
2023-06-23 01:43:12 +0200

    Task19

---
+++ Desktop/HomeworkC#_03/Task19/Program.cs
@@ -0,0 +1,44 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+int num = Promt("Введите пятизначное число: ");
+int reversenum = Reverse(num);
+CheckPalindrom(num);
+
+
+int Promt(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+int Reverse(int number)
+{
+        int reverse = 0;
+        if(number > 9999 && number < 100000)
+        {
+            for(int i = 0; i < 5; i++) 
+            {
+                reverse = reverse * 10 + number % 10;
+                number = number / 10;
+            }
+                
+                //Console.WriteLine(reverse);
+
+        }
+    else Console.WriteLine("Число не является пятизначным! ");
+    return reverse;
+}
+
+void CheckPalindrom(int value)
+{
+    if(value > 9999 && value < 100000)
+    {
+        if(reversenum == value) {Console.WriteLine(value + " -> да");}
+        else Console.WriteLine(value + " -> нет");
+    }
+    else Console.WriteLine("Введите пятизначное число: ");
+}
