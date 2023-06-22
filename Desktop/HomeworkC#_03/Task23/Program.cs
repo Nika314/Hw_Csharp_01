5176b95ab394fd59c9319c6f37da1a71fd3b6317
AnnaTsygalnyuk <nika314@list.ru>
2023-06-23 01:45:57 +0200

    Task23

---
+++ Desktop/HomeworkC#_03/Task23/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int num = Promt("Введите натуральное число N: ");
+double cubeNum = Cube(num);
+
+
+double Cube(int number)
+{
+    double cube = 1;
+    if(number > 0)
+    {
+        for(int i = 1; i <= number; i++)
+        {
+            cube = Math.Pow(i, 3);
+            Console.Write(cube + " ");
+        }
+    
+    }
+    else Console.WriteLine("Число не является натуральным!");
+    return cube;
+}
+
+
+int Promt(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
