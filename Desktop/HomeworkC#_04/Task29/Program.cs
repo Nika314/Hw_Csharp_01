24f7d517d5c4b23127876ae80dee14c9b3f288d6
AnnaTsygalnyuk <nika314@list.ru>
2023-06-26 21:36:21 +0200

    Task29

---
+++ Desktop/HomeworkC#_04/Task29/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int size = 8;
+int minValue = Prompt("Введите минимальное число диапазона массива: ");
+int maxValue = Prompt("Введите максимальное число диапазона массива: ");
+int[] array = GetArray(size, minValue, maxValue);
+PrintArray(array);
+
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+
+int[] GetArray(int size, int minValue, int maxValu)
+{
+    int[] result = new int[size];
+    for(int i =0; i < result.Length; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1); 
+    }
+    return result;
+}
+void PrintArray(int[] arr)
+{
+    for(int i =0; i < arr.Length - 1; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.Write($"{arr[arr.Length-1]} -> ");
+    Console.Write("[");
+    for(int i =0; i < arr.Length - 1; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.Write($"{arr[arr.Length-1]}"); 
+    Console.Write("]");
+}
+
